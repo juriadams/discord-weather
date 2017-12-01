e0d8a872f51dba1de47182eab026455384309e88
Juri Adams <juri@4da.ms>
2017-12-01 19:36:11 +0100

    added temperature icon

--- app.js
+++ app.js
@@ -46,7 +46,6 @@ client.on('message', msg => {
         var flag = ":flag_" + info.sys.country.toLowerCase() + ":"
 
         // Really long section for specifying an emoji fitting the the current weather state
-
         var weather_icon;
 
         if ( weather.icon == "01d") {
@@ -143,6 +142,37 @@ client.on('message', msg => {
           var weather_icon = ":fog:";
         }
 
+        // And once again for the sake of the Temperature
+        var temp_icon;
+
+        if ( info.main.temp < -10 ) {
+          temp_icon = ":snowman2:";
+        }
+
+        if ( info.main.temp < 0 ) {
+          temp_icon = ":snowman:";
+        }
+
+        if ( info.main.temp < 10 ) {
+          temp_icon = ":cloud_snow:";
+        }
+
+        if ( info.main.temp < 20 ) {
+          temp_icon = ":white_sun_cloud:";
+        }
+
+        if ( info.main.temp < 30 ) {
+          temp_icon = ":sunny:";
+        }
+
+        if ( info.main.temp < 40 ) {
+          temp_icon = ":thermometer:";
+        }
+
+        if ( info.main.temp > 40) {
+          temp_icon ":thermometer:";
+        }
+
         // Message layout for the bot's response
         let embed = {
           title: "__Kachelmann Weather Report__",
@@ -151,7 +181,7 @@ client.on('message', msg => {
           color: 0x8DE969,
           fields: [
             { name: "__Current Weather:__", value: "**" + weather.main + " " +  weather_icon + "**\n*(" + weather.description + ")*", inline: true },
-            { name: "__Current Temperature:__", value: "It's currently **" + info.main.temp + "°C** \n*(" + info.main.temp_min + "°C to " + info.main.temp_max + "°C)*", inline: true },
+            { name: "__Current Temperature:__", value: "It's currently **" + info.main.temp + "°C** " + temp_icon + " \n*(" + info.main.temp_min + "°C ~ " + info.main.temp_max + "°C)*", inline: true },
             { name: "__Sunrise and Sunset:__", value: "Sunrise at **" + sunrise + " CET** :sunny:️\nSunset at **" + sunset + " CET** :crescent_moon:"}
           ],
           footer: { text: "[" + moment().format('LTS') + "] Kachelmann Bot | GitHub: 4dams/Kachelmann"}
